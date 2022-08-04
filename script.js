1ce0ecd9fb67cd88165de8e4934a3340d0848e1e
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-04 22:36:09 +0530

    Color Flipper - Script: Change Background Color

--- script.js
+++ script.js
@@ -2,3 +2,18 @@
 console.clear();
 // Color Flipper
 
+const colors = ['#03A64A', '#025940', 'rgba(2, 64, 89)', '#04BF8A'];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function () {
+	const randomNumber = getRandomNumber();
+	console.log(getRandomNumber());
+
+	document.body.style.backgroundColor = colors[randomNumber];
+	color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+	return Math.floor(Math.random() * colors.length);
+}
