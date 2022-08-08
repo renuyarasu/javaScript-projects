a3cad1bc1a08667fc72581ae9ec987c74825b068
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-08 20:25:11 +0530

    Counter - counter functionality

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 // JavaScript Projects
 console.clear();
 
+let count = 0;
+const value = document.getElementById('value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains('decrease')) {
+            count--;
+        } else if (styles.contains('increase')) {
+            count++;
+        } else {
+            count = 0;
+        }
+        value.textContent = count;
+    })
+})
